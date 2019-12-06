234b63b1d14abb7185875cae71f1f2f1c2d046e0
thanhtv4 <thanhtv4@topica.edu.vn>
2019-12-06 16:00:50 +0700

    update

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import axios from 'axios'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 export default class ProductListing extends React.Component {
     constructor(props) {
         super(props);
@@ -28,12 +34,50 @@ export default class ProductListing extends React.Component {
     render() {
         const { products } = this.state;
         const listItems = products.map((product) =>
-        <li>{product.name}- {product.sku}</li>
+        <div class="product">
+            <div className="product-avatar" >
+                <img src={product.images[0].url} alt={product.name}/>
+            </div>
+            <div className="product-info">
+                <div className="product-name">
+                {product.name}
+                </div>
+                <div className="product-sell-price">
+                    {product.price.sellPrice.toLocaleString('it-IT', {
+                        style: 'currency',
+                        currency: 'VND'
+                    })}
+                </div>
+                <div className="product-old-price-wrap">
+                    <div className="product-old-price">
+                        {product.price.supplierSalePrice.toLocaleString('it-IT', {
+                        style: 'currency',
+                        currency: 'VND'
+                    })}
+                    </div> 
+                    <span className="sale-percent">-20%</span>
+                </div>
+                {/* {
+                    product.price.supplierSalePrice > product.price.sellPrice ?
+                    <div className="product-old-price">
+                        {product.price.supplierSalePrice}
+                        <span className="sale-percent">20%</span>
+                    </div> :
+                    null
+                } */}
+                
+            </div>
+
+            
+        </div>
         );
         return (
             <div>
-                ProductListing
-                <ul>{listItems}</ul>
+                <header>
+                    <button className="back">Back</button>
+                    <input type="text" placeholder="Nhập tên, mã sản phẩm" />
+                </header>
+                <div className="product-wrapper">{listItems}</div>
             </div>
         )
     }
